2633da05a759319c66f8da42dc38cf1a5ce2ab6b
nishal <nishal@gmail.com>
2025-07-19 22:35:25 +0500

    Assignment Part 1 remaining part

--- HealthProfessional.java
+++ HealthProfessional.java
@@ -4,4 +4,18 @@ public class HealthProfessional {
     String name;
     String doctorType;
 
+    public HealthProfessional() {
+    }
+
+    public HealthProfessional(int ID, String Name, String DoctorType) {
+        this.ID = ID;
+        this.name = Name;
+        this.doctorType = DoctorType;
+    }
+
+    public void printDetails() {
+        System.out.println("ID: " + ID);
+        System.out.println("Name: " + name);
+        System.out.println("Type: " + doctorType);
+    }
 }
